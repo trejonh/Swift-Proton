0dec723e2cc7c1c092cd40c4f0f024a5a85e9c86
trejonh <houset@msoe.edu>
2017-04-13 14:43:33 -0500

    minor

--- motors.py
+++ motors.py
@@ -15,18 +15,18 @@ GPIO.setup(Motor1A,GPIO.OUT)
 GPIO.setup(Motor1B,GPIO.OUT)
 GPIO.setup(Motor1E,GPIO.OUT)
  
-#GPIO.setup(Motor2A,GPIO.OUT)
-#GPIO.setup(Motor2B,GPIO.OUT)
-#GPIO.setup(Motor2E,GPIO.OUT)
+GPIO.setup(Motor2A,GPIO.OUT)
+GPIO.setup(Motor2B,GPIO.OUT)
+GPIO.setup(Motor2E,GPIO.OUT)
  
 print "Going forwards, clockwise"
 GPIO.output(Motor1A,GPIO.HIGH)
 GPIO.output(Motor1B,GPIO.LOW)
 GPIO.output(Motor1E,GPIO.HIGH)
  
-#GPIO.output(Motor2A,GPIO.HIGH)
-#GPIO.output(Motor2B,GPIO.LOW)
-#GPIO.output(Motor2E,GPIO.HIGH)
+GPIO.output(Motor2A,GPIO.HIGH)
+GPIO.output(Motor2B,GPIO.LOW)
+GPIO.output(Motor2E,GPIO.HIGH)
  
 sleep(2)
 
@@ -37,14 +37,14 @@ GPIO.output(Motor1B,GPIO.HIGH)
 GPIO.output(Motor1A,GPIO.LOW)
 GPIO.output(Motor1E,GPIO.HIGH)
  
-#GPIO.output(Motor2A,GPIO.LOW)
-#GPIO.output(Motor2B,GPIO.HIGH)
-#GPIO.output(Motor2E,GPIO.HIGH)
+GPIO.output(Motor2A,GPIO.LOW)
+GPIO.output(Motor2B,GPIO.HIGH)
+GPIO.output(Motor2E,GPIO.HIGH)
  
 sleep(2)
  
 print "Now stop"
 GPIO.output(Motor1E,GPIO.LOW)
-#GPIO.output(Motor2E,GPIO.LOW)
+GPIO.output(Motor2E,GPIO.LOW)
  
 GPIO.cleanup()
