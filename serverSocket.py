88209b0b32276f1f9bb557bf01077362971cbb05
trejonh <houset@msoe.edu>
2017-05-13 14:04:50 -0500

    blietooth server

--- serverSocket.py
+++ serverSocket.py
@@ -1,6 +1,6 @@
 #create an INET, STREAMing socket
-serversocket = socket.socket(
-    socket.AF_INET, socket.SOCK_STREAM)
+import socket
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #bind the socket to a public host,
 # and a well-known port
 serversocket.bind((socket.gethostname(), 9000))
