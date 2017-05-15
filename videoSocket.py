e0d682ee91cc3a3bd8cedba159afa9bf6629d220
trejonh <houset@msoe.edu>
2017-05-14 23:52:22 -0500

    server socket

---
+++ videoSocket.py
@@ -0,0 +1,24 @@
+import cv2
+import numpy as np
+import socket
+import sys
+import pickle
+import struct
+
+cap = cv2.VideoCapture(0)
+HOST = ''                 # Symbolic name meaning all available interfaces
+PORT = 9000              # Arbitrary non-privileged port
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen(1)
+conn, addr = s.accept()
+while True:
+    ret,frame = cap.read()
+    data = pickle.dumps(frame)
+	print lens(data)
+	try:
+		s.sendall(struct.pack("L", len(data)) + data)
+	except:
+		break
+s.close()
+cap.release()
