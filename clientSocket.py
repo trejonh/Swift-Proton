46b18bc9d051d23876d47b13a1436136229ae4fd
trejonh <houset@msoe.edu>
2017-05-14 21:29:33 -0500

    print data

--- clientSocket.py
+++ clientSocket.py
@@ -1,7 +1,7 @@
 # Echo client program
 import socket
 
-HOST = '192.168.0.1'    # The remote host
+HOST = '192.168.0.18'    # The remote host
 PORT = 9000              # The same port as used by the server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
