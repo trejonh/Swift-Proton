5fd148fc36c2e403e34a5ad3db621f4e8350b9d8
trejonh <houset@msoe.edu>
2017-05-16 23:53:33 -0500

    raspivid

--- video.sh
+++ video.sh
@@ -1,2 +1,1 @@
-echo "in side script about to run"
-raspivid -o - -t 0 -w 1920 -h 1080 -fps 30|cvlc stream:///dev/stdin --sout '#standard{access=http,mux=ts,dst=:9000}' :demux=h264
+raspivid -o - -t 0 -w 1080 -h 720 -fps 30|cvlc stream:///dev/stdin --sout '#standard{access=http,mux=ts,dst=:9000}' :demux=h264
