d276abfefa738361fbface51217a684e747e1a6f
trejonh <houset@msoe.edu>
2017-05-17 00:46:23 -0500

    raspivid

--- video.sh
+++ video.sh
@@ -1,2 +1,1 @@
-raspivid -n -w 1280 -h 720 -b 4500000 -fps 30 -vf -hf -t 0 -o - | \
-cvlc -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:9000/}' :demux=h264
+raspivid -o - -t 0 -w 1280 -h 720 -fps 30|cvlc stream:///dev/stdin --sout '#rtp{sdp=rtsp://:9000/}' :demux=h264
