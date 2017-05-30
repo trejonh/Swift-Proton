a190c78fc3e602da306975f3ffca6dc2c10f507c
trejonh <houset@msoe.edu>
2017-05-30 17:32:03 -0500

    raspivid

--- ffmpegInstall.sh
+++ ffmpegInstall.sh
@@ -1,50 +1,20 @@
+sudo deb http://www.deb-multimedia.org jessie main non-free deb-src http://www.deb-multimedia.org jessie main non-free
 sudo apt-get update
-sudo apt-get -y install autoconf automake build-essential libass-dev libfreetype6-dev \
-  libsdl2-dev libtheora-dev libtool libva-dev libvdpau-dev libvorbis-dev libxcb1-dev libxcb-shm0-dev \
-  libxcb-xfixes0-dev pkg-config texinfo zlib1g-dev
- mkdir ~/ffmpeg_sources
- sudo apt-get install yasm
- cd ~/ffmpeg_sources
-wget http://www.tortall.net/projects/yasm/releases/yasm-1.3.0.tar.gz
-tar xzvf yasm-1.3.0.tar.gz
-cd yasm-1.3.0
-./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin"
+sudo apt-get install deb-multimedia-keyring
+sudo apt-get update
+sudo apt-get remove ffmpeg
+sudo apt-get install build-essential libmp3lame-dev libvorbis-dev libtheora-dev libspeex-dev yasm pkg-config libfaac-dev libopenjpeg-dev libx264-dev
+mkdir software
+cd software
+wget http://ffmpeg.org/releases/ffmpeg-2.7.2.tar.bz2
+cd ..
+mkdir src
+cd src
+tar xvjf ../software/ffmpeg-2.7.2.tar.bz2
+cd ffmpeg-2.7.2
+./configure --enable-gpl --enable-postproc --enable-swscale --enable-avfilter --enable-libmp3lame 
+--enable-libvorbis --enable-libtheora --enable-libx264 --enable-libspeex --enable-shared 
+--enable-pthreads --enable-libopenjpeg --enable-libfaac --enable-nonfree
 make
-make install
-sudo apt-get install libx264-dev
-cd ~/ffmpeg_sources
-wget http://download.videolan.org/pub/x264/snapshots/last_x264.tar.bz2
-tar xjvf last_x264.tar.bz2
-cd x264-snapshot*
-PATH="$HOME/bin:$PATH" ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --enable-static --disable-opencl
-PATH="$HOME/bin:$PATH" make
-make install
-sudo apt-get install libx265-dev
-sudo apt-get install cmake mercurial
-cd ~/ffmpeg_sources
-hg clone https://bitbucket.org/multicoreware/x265
-cd ~/ffmpeg_sources/x265/build/linux
-PATH="$HOME/bin:$PATH" cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$HOME/ffmpeg_build" -DENABLE_SHARED:bool=off ../../source
-make
-make install
-cd ~/ffmpeg_sources
-wget http://ffmpeg.org/releases/ffmpeg-snapshot.tar.bz2
-tar xjvf ffmpeg-snapshot.tar.bz2
-cd ffmpeg
-PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure \
-  --prefix="$HOME/ffmpeg_build" \
-  --pkg-config-flags="--static" \
-  --extra-cflags="-I$HOME/ffmpeg_build/include" \
-  --extra-ldflags="-L$HOME/ffmpeg_build/lib" \
-  --bindir="$HOME/bin" \
-  --enable-gpl \
-  --enable-libass \
-  --enable-libfreetype \
-  --enable-libtheora \
-  --enable-libvorbis \
-  --enable-libx264 \
-  --enable-libx265 \
-  --enable-nonfree
-PATH="$HOME/bin:$PATH" make
-make install
-hash -r
+sudo make install
+sudo ldconfig
